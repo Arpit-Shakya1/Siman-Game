72b52a245d752976c4d3ec843ca25870e629fb14
Arpit <arpitshakya1372007@gmail.com>
2026-06-12 15:15:07 +0530

    add.file

---
+++ app.js
@@ -0,0 +1,88 @@
+let userSeq = [];
+let gameSeq= [];
+let level = 0;
+ let started = false;
+ let h2 = document.querySelector("h2");
+ let btns = ["red", "yellow", "green", "purple"];
+
+ document.addEventListener("keypress", function(){
+    if(started == false){
+        console.log("Game Started");
+        started = true;
+        levelUp();
+    }
+ });
+
+
+ function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+
+    },250);
+ }
+
+ function userFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250)
+ }
+
+
+ function levelUp(){
+   userSeq = [];
+    level++;
+    h2.innerText = `level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let btn = document.querySelector(`.${randColor}`);
+    gameFlash(btn);
+    gameSeq.push(randColor);
+
+
+ }
+
+ function ansKey(idx){
+    if (gameSeq[idx]=== userSeq[idx]){
+        if(gameSeq.length == userSeq.length){
+            setTimeout(levelUp , 1000);
+        }
+    }
+    else{
+        h2.innerHTML = `Game over! <b>Your score was${level}<b> <br> Press any key to start game`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        },150);
+        reset();
+    }
+ }
+
+
+
+ function keypress(){
+    let btn = this;
+    userFlash(btn);
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    ansKey(userSeq.length-1);
+
+
+
+    
+ }
+
+
+ let allBtns = document.querySelectorAll(".btn");
+ for(btn of allBtns){
+    btn.addEventListener("click" , keypress);
+ }
+
+ function reset(){
+    gameSeq = [];
+     userSeq = [];
+      level = 0;
+       started = false;
+ }
